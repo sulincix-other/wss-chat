1cb7534ed201136fdaabd605eeb631206884d22b
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2025-03-25 08:38:17 +0000

    add route room

--- main.py
+++ main.py
@@ -2,38 +2,46 @@
 
 """Echo server using the threading API with SSL."""
 
+import os
 import asyncio
 import uuid
 import ssl
 from websockets import serve
 
-connected_clients = []
+connected_clients = {}
 
 async def echo(websocket):
+    path = websocket.request.path
     websocket.id = str(uuid.uuid4())
-    print("CONNECT:" + websocket.id)
-    connected_clients.append(websocket)
+    print("CONNECT:{}:{}".format(path, websocket.id))
+    if path not in connected_clients:
+        connected_clients[path] = []
+    connected_clients[path].append(websocket)
     try:
         async for message in websocket:
-            print("MESSAGE:" + websocket.id)
-            for client in connected_clients:
+            print("MESSAGE:{}:{}".format(path, websocket.id))
+            for client in connected_clients[path]:
                 if client.id == websocket.id:
                     continue
                 await client.send(message)
     except Exception as e:
         print(f"Error: {e}")
     finally:
-        print("DISCONNECT:" + websocket.id)
-        connected_clients.remove(websocket)
+        print("DISCONNECT:{}:{}".format(path, websocket.id))
+        connected_clients[path].remove(websocket)
 
 async def main():
-    # Create SSL context
-    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    ssl_context.load_cert_chain(certfile="cert.pem", keyfile="key.pem")
+    if os.path.isfile("cert.pem") and os.path.isfile("key.pem"):
+        # Create SSL context
+        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        ssl_context.load_cert_chain(certfile="cert.pem", keyfile="key.pem")
 
-    # Start the server with SSL
-    async with serve(echo, "0.0.0.0", 8765, ssl=ssl_context) as server:
-        await server.wait_closed()
+        # Start the server with SSL
+        async with serve(echo, "0.0.0.0", 8765, ssl=ssl_context) as server:
+            await server.wait_closed()
+    else:
+        async with serve(echo, "0.0.0.0", 8765) as server:
+            await server.wait_closed()
 
 if __name__ == "__main__":
     asyncio.run(main())
