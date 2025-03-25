7072706a9ee706be73ca0bf2944ca6c4cc55cdf0
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2025-03-25 08:45:01 +0000

    http server improvements

--- server.py
+++ server.py
@@ -1,18 +1,36 @@
-import http.server
+#!/usr/bin/env python3
+
+# Python 3 server
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
 import ssl
+import os
 
-# Define the server address and port
-server_address = ('0.0.0.0', 4443)  # Listen on all interfaces
+hostName = "0.0.0.0"
+serverPort = 4443
 
-# Create a simple HTTP server
-httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
+class Server(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        if self.path.startswith("/audio"):
+            with open("audio.html","rb") as f:
+                self.wfile.write(f.read())
+        else:
+            with open("message.html","rb") as f:
+                self.wfile.write(f.read())
 
-# Create an SSL context
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain(certfile='cert.pem', keyfile='key.pem')
+httpd = HTTPServer((hostName, serverPort), Server)
 
-# Wrap the server socket with SSL
-httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+if os.path.isfile("cert.pem") and os.path.isfile("key.pem"):
+    # Create an SSL context
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain(certfile='cert.pem', keyfile='key.pem')
+
+    # Wrap the server socket with SSL
+    httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
 
 print("Serving on https://0.0.0.0:4443")
 httpd.serve_forever()
