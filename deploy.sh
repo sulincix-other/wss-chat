efe545e054f0fed37209ddd757596343e5ab3e14
Ali Riza KESKIN <aliriza.keskin@pardus.org.tr>
2025-03-25 09:08:12 +0000

    ssl create lazy way

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ if [ ! -d venv ] ; then
     python3 -m venv venv
     source venv/bin/activate
     pip3 install websockets
-    openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -sha256 -days 365
+    openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 10000 -nodes
 fi
 source venv/bin/activate
 python3 main.py
